cc0e6e4b41e366eaa07face54883d4eb97e9f16a
codeconcept <contact@codeconcept.fr>
2020-05-05 11:58:54 +0200

    Retrieving image

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const createAdForm = document.forms.createad;
+const fileUpload = createAdForm.adpicture;
+
+fileUpload.addEventListener("change", getImage);
+let fileImage = null;
 
 createAdForm.addEventListener("submit", createAd);
 
@@ -6,5 +10,11 @@ function createAd(e) {
   e.preventDefault();
   const title = createAdForm.adtitle.value.trim();
   const category = createAdForm.adcategory.value;
-  console.log(title, category);
+  const picture = fileImage;
+  console.log(title, category, picture);
+}
+
+function getImage(params) {
+  console.log("images", this.files[0]);
+  fileImage = this.files[0];
 }
